c1f6397922aba83990fea758b11d290f04a0a175
buchonsi <buchonsi@naver.com>
2023-07-06 16:42:59 +0900

    init commit

---
+++ src/main/java/com/yoons/managediet/ManageDietApplication.java
@@ -0,0 +1,13 @@
+package com.yoons.managediet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageDietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageDietApplication.class, args);
+    }
+
+}
