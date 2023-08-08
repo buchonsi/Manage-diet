d2f166c7ed8b2112c280f14517ac2f671c300410
buchonsi <buchonsi@naver.com>
2023-08-09 01:57:38 +0900

    controller response타입 변경
    ResponseEntity 사용

--- src/main/java/com/yoons/managediet/recipe/controller/RecipeRecommendController.java
+++ src/main/java/com/yoons/managediet/recipe/controller/RecipeRecommendController.java
@@ -1,14 +1,12 @@
 package com.yoons.managediet.recipe.controller;
 
 import com.yoons.managediet.recipe.dto.InputDto;
-import com.yoons.managediet.recipe.dto.RecipeDto;
 import com.yoons.managediet.recipe.service.RecipeRecommendService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +14,8 @@ public class RecipeRecommendController {
 
     private final RecipeRecommendService recipeRecommendService;
 
-    //@Todo return 양식 통일 되도록 만들기
     @GetMapping("/recipe/search/calorie")
-    public List<RecipeDto> getRecommendRecipeList(@RequestBody InputDto inputDto) {
-        return recipeRecommendService.recommendRecipeList(inputDto.getCalorie());
+    public ResponseEntity<Object> getRecommendRecipeList(@RequestBody InputDto inputDto) {
+        return ResponseEntity.ok(recipeRecommendService.recommendRecipeList(inputDto.getCalorie()));
     }
 }
