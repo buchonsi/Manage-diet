52cfac30df3b9af191778dc87f6c260219d93e78
buchonsi <buchonsi@naver.com>
2023-09-27 14:41:37 +0900

    swagger-ui 파라미터 설정

--- src/main/java/com/yoons/managediet/diet/controller/DietController.java
+++ src/main/java/com/yoons/managediet/diet/controller/DietController.java
@@ -1,18 +1,16 @@
 package com.yoons.managediet.diet.controller;
 
 import com.yoons.managediet.diet.dto.DailyOutputDto;
-import com.yoons.managediet.diet.dto.DietSaveInputDto;
 import com.yoons.managediet.diet.dto.DietOutputDto;
+import com.yoons.managediet.diet.dto.DietSaveInputDto;
 import com.yoons.managediet.diet.dto.DietSearchInputDto;
-import com.yoons.managediet.diet.entity.TypeOfTime;
 import com.yoons.managediet.diet.service.AnalyzeDailyDietService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 
 @Slf4j
 @RestController
@@ -20,13 +18,20 @@ import java.time.LocalDate;
 public class DietController {
     private final AnalyzeDailyDietService analyzeDailyDietService;
 
+    @Parameter(name = "date", description = "date", required = true)
     @GetMapping("/diet/daily")
-    public ResponseEntity<DailyOutputDto> getOneDayDiet(@ModelAttribute DietSearchInputDto dietSearchInputDto) {
+    public ResponseEntity<DailyOutputDto> getOneDayDiet(
+            @Parameter(hidden = true) @ModelAttribute DietSearchInputDto dietSearchInputDto) {
         return ResponseEntity.ok(analyzeDailyDietService.getOneDayDiet(dietSearchInputDto.getDate()));
     }
 
+    @Parameters({
+            @Parameter(name = "date", description = "date", required = true),
+            @Parameter(name = "time", description = "time", required = true)
+    })
     @GetMapping("/diet/daily/time")
-    public ResponseEntity<DietOutputDto> getPartDiet(@ModelAttribute DietSearchInputDto dietSearchInputDto) {
+    public ResponseEntity<DietOutputDto> getPartDiet(
+            @Parameter(hidden = true) @ModelAttribute DietSearchInputDto dietSearchInputDto) {
         return ResponseEntity.ok(analyzeDailyDietService.getPartDiet(dietSearchInputDto.getDate(), dietSearchInputDto.getTime()));
     }
 
